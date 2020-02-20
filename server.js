25eba1cab00533e5eb64de617ee44fbfa1d0fd97
Kamshat Mussina <mussinak777@gmail.com>
2020-02-19 19:35:20 -0700

    added components, utils folders

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,9 +14,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/icecream");
-
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
